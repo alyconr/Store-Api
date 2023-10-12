e84b4cb197508fa9cf0aefcf8ca1d13c0cdad3b5
alyconr <alyconr@hotmail.com>
2023-10-12 12:49:07 -0500

    :construction: Building models

---
+++ models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
