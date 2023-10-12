aea4a32bde922669532ddae987f9dd8b2e60fb14
alyconr <alyconr@hotmail.com>
2023-10-12 12:51:09 -0500

    :sparkles:  Configure routes

---
+++ routes/products.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    getAllProductsStatic,
+    getAllProducts,
+} = require("../controllers/products"); 
+
+router.route("/static").get(getAllProductsStatic);
+router.route("/").get(getAllProducts);
+
+module.exports = router;
+
+
