902fb5a6ef6f458fb1bf62bec14fc75ea1d21add
alyconr <alyconr@hotmail.com>
2023-10-12 21:40:39 -0500

    :sparkles: Addin fields and numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,71 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({name: "sofa set"});
-  res.status(200).json({ products, nbHits: products.length }); 
-}
+  const products = await Product.find({ name: "sofa set" });
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
-}
- 
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g; // this is a regular expression that will match the operator in the string and replace it with the operator in the operatorMap object, \b is a word boundary that means that it will match at the beginning or end of a word and \g is a group that will match all occurrences of the regular expression
+
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1; // this means that page is 1
+  const limit = Number(req.query.limit) || 10; // this means that limit is 10
+  const startIndex = (page - 1) * limit; // this means 1 - 1 * 10 = 0
+
+  result = result.skip(startIndex).limit(limit); // this  that the result will skip 0 and limit 10
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
+};
+
 module.exports = {
   getAllProductsStatic,
-  getAllProducts
-}
+  getAllProducts,
+};
